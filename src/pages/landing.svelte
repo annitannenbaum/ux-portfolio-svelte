8bd365a823bce4ec837de094dd2e1013671a2e02
Anni Tannenbaum <annahoege@gmx.de>
2021-10-03 13:57:29 +0200

    added about content and routing to other pages

--- src/pages/landing.svelte
+++ src/pages/landing.svelte
@@ -1,9 +1,7 @@
-<script>
-
-</script>
+<script></script>
 
 <main>
-    <h1>Hi,</h1>
+    <h1>Hi, <img src="/assets/smiley.svg" alt="smiley"></h1>
     <h1>I'm Anka & I do</h1>
     <h2>UI/UX & Visual Design</h2>
 </main>
@@ -13,20 +11,6 @@
 		padding: 6rem;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: white;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	h2 {
-		color: transparent;
-		font-size: 4em;
-		font-weight: 700;
-		-webkit-text-stroke: 1px;
-		-webkit-text-stroke-color: white;
 	}
 
 	@media (min-width: 640px) {
